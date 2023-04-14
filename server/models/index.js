1a35825e625960b9d3584a5c32f5eeb845987c4e
Rashida Kapadia <rashidamk21@gmail.com>
2023-04-14 12:21:16 -0500

    Sign up schema done

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const Cook = require('./Cook');
 const Dish = require('./Dish');
 const MenuItem = require('./MenuItem');
+const Signup = require('./Signup');
+const User = require('./User');
 
-module.exports = { Cook, Dish, MenuItem };
+module.exports = { Cook, Dish, MenuItem, Signup, User };
